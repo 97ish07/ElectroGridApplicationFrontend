be2e65ce0dfe0055da5bfefdd98f759e93310cc6
97ish07 <ishani9799@gmail.com>
2022-05-15 13:04:04 +0530

    import java utils.hashmap

--- src/main/java/model/InquiryAPI.java
+++ src/main/java/model/InquiryAPI.java
@@ -1,6 +1,11 @@
 package model;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,12 +42,38 @@ public class InquiryAPI extends HttpServlet {
        response.getWriter().write(output); 
 		
 	}
+	
+	// Convert request parameters to a Map
+		private static Map getParasMap(HttpServletRequest request)
+		{
+			Map<String, String> map = new HashMap<String, String>();
+		
+			try
+			{
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ?
+						scanner.useDelimiter("\\A").next() : "";
+				scanner.close();
+				String[] params = queryString.split("&");
+				 
+				for (String param : params)
+				{ 
+					 String[] p = param.split("=");
+					 map.put(p[0], p[1]);
+				}
+			 }
+			catch (Exception e)
+			{
+			}
+			return map;
+			}
 
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
+	
+	
+	
+
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
