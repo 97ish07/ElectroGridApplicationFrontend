ae3dd995015ce706501af92c6a8c8939cf028cbf
97ish07 <ishani9799@gmail.com>
2022-05-15 11:42:17 +0530

    2nd update of InquiryAPI.java file

--- src/main/java/model/InquiryAPI.java
+++ src/main/java/model/InquiryAPI.java
@@ -11,22 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/InquiryAPI")
 public class InquiryAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	Inquiry inquiryObj = new Inquiry();
        
    
     public InquiryAPI() {
         super();
-        // TODO Auto-generated constructor stub
-    }
-
+     
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
